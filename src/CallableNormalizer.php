dc65c47a78659d56a3271f117ac1392123e5d538
Andreas Möller <am@localheinz.com>
2018-01-12 16:26:06 +0100

    Enhancement: Implement CallableNormalizer

---
+++ src/CallableNormalizer.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2018 Andreas Möller.
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/localheinz/json-normalizer
+ */
+
+namespace Localheinz\Json\Normalizer;
+
+final class CallableNormalizer implements NormalizerInterface
+{
+    /**
+     * @var callable
+     */
+    private $callable;
+
+    public function __construct(callable $callable)
+    {
+        $this->callable = $callable;
+    }
+
+    public function normalize(string $json): string
+    {
+        if (null === \json_decode($json) && JSON_ERROR_NONE !== \json_last_error()) {
+            throw new \InvalidArgumentException(\sprintf(
+                '"%s" is not valid JSON.',
+                $json
+            ));
+        }
+
+        $callable = $this->callable;
+
+        return $callable($json);
+    }
+}
