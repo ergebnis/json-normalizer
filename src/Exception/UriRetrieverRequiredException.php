a1d43dc1c69a908d697769c90a1f1032a51587a4
Andreas Möller <am@localheinz.com>
2018-11-04 13:21:30 +0100

    Enhancement: Implement ChainUriRetriever

---
+++ src/Exception/UriRetrieverRequiredException.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2018 Andreas Möller.
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/localheinz/json-normalizer
+ */
+
+namespace Localheinz\Json\Normalizer\Exception;
+
+final class UriRetrieverRequiredException extends \InvalidArgumentException implements ExceptionInterface
+{
+    public static function create(): self
+    {
+        return new self('Cannot retrieve URIs when no retrievers have been injected.');
+    }
+}
---
+++ test/Unit/Exception/UriRetrieverRequiredExceptionTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2018 Andreas Möller.
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/localheinz/json-normalizer
+ */
+
+namespace Localheinz\Json\Normalizer\Test\Unit\Exception;
+
+use Localheinz\Json\Normalizer\Exception\InvalidNewLineStringException;
+use Localheinz\Json\Normalizer\Exception\UriRetrieverRequiredException;
+
+/**
+ * @internal
+ */
+final class UriRetrieverRequiredExceptionTest extends AbstractExceptionTestCase
+{
+    public function testExtendsInvalidArgumentException(): void
+    {
+        $this->assertClassExtends(\InvalidArgumentException::class, InvalidNewLineStringException::class);
+    }
+
+    public function testCreateReturnsUriRetrieverRequiredException(): void
+    {
+        $exception = UriRetrieverRequiredException::create();
+
+        $this->assertInstanceOf(UriRetrieverRequiredException::class, $exception);
+        $this->assertSame('Cannot retrieve URIs when no retrievers have been injected.', $exception->getMessage());
+    }
+}
