e079d00aaebb93d4d591034bbb2a3cc1f49dcc3a
Andreas Möller <am@localheinz.com>
2022-12-29 10:16:51 +0100

    Enhancement: Add workflow for updating schema.json

---
+++ bin/laxify-schema.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2018-2022 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/composer-normalize
+ */
+
+use Ergebnis\Json\Normalizer\Format;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$schemaFile = __DIR__ . '/../test/Fixture/Vendor/Composer/schema.json';
+
+$schema = \json_decode(
+    \file_get_contents($schemaFile),
+    false,
+);
+
+$schema->additionalProperties = true;
+$schema->required = [];
+
+\file_put_contents($schemaFile, \json_encode(
+    $schema,
+    Format\JsonEncodeOptions::default()->toInt()
+));
